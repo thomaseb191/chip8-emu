a4a32fbde7fac1302050dcd42bdb01347f8c23ed
Thomas <10285945+thomaseb191@users.noreply.github.com>
2020-02-27 17:37:53 -0500

    added load game, untested

--- chip8.cpp
+++ chip8.cpp
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "chip8.h"
 
 /*
@@ -39,6 +40,47 @@ void chip8::initialize() {
  */
 void chip8::loadgame(char* filename){
 
+    printf("Loading: %s\n", filename);
+
+    //Check file exists
+    FILE * rom = fopen(filename, "rb");
+    if (rom == NULL){
+        fprintf(stderr, "File error");
+        return;
+    }
+
+    //Check file size
+    fseek(rom, 0, SEEK_END);
+    long romsize = ftell(rom);
+    rewind(rom);
+    printf("Filesize: %d\n", (int)romsize);
+
+    //Allocate memory to hold file
+    char* buffer = (char*)malloc(sizeof(char) * romsize);
+    if (buffer == NULL){
+        fprintf(stderr, "Memory error");
+        return;
+    }
+
+    //Copy file into buffer
+    size_t result = fread(buffer, 1, romsize, rom);
+    if (result != romsize) {
+        fprintf(stderr, "Reading error");
+        return;
+    }
+
+    //Copy buffer into chip8 mem
+    if((4096-512) > romsize){
+        for (int i = 0; i < romsize; i++)
+            memory[i + 0x200] = buffer[i]; //Offset for program
+    } else {
+        printf("Error: ROM too big for memory");
+    }
+
+    fclose(rom);
+    free(buffer);
+
+    return;
 }
 
 /*
