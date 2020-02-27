41157da2990f51c6da5ec76fb7a2294f1f84c3be
Thomas <10285945+thomaseb191@users.noreply.github.com>
2020-02-27 16:50:13 -0500

    defined chip8 class

--- chip8.h
+++ chip8.h
@@ -2,4 +2,22 @@ class chip8 {
     public:
         chip8();
         ~chip8();
+
+        void initialize();
+        void loadgame();
+        void emulateCycle();
+
+    private:
+        unsigned short opcode; //Represents one of 35 opcodes
+
+        unsigned char memory[4096]; //4K worth of memory
+        unsigned char V[16]; //15 8-bit general use registers, 1 carry flag register
+
+        unsigned short I; //Index register
+        unsigned short pc; //Program counter
+
+        unsigned char gfx[64 * 32]; //Screen is 64x32 pixels
+
+        unsigned char delay_timer;
+        unsigned char sound_timer;
 };
